d2f75ab694bb549fc6ea6cfd31968c3e22a6dc2c
Yichuan Wang <133667618+Charlie-Wang-03@users.noreply.github.com>
2026-09-19 20:24:50 +0800

    JSP-000404: export phase turn-slot closure

--- submissions/jsp-000404-wip-charlie/JSP000404Research.lean
+++ submissions/jsp-000404-wip-charlie/JSP000404Research.lean
@@ -61,6 +61,8 @@ import JSP000404Research.BoundaryIntervalCount
 
 import JSP000404Research.HullTurnBudget
 
+import JSP000404Research.PhaseTurnClosure
+
 import JSP000404Research.AdaptiveDirectionColor
 
 import JSP000404Research.DeficitTwo
