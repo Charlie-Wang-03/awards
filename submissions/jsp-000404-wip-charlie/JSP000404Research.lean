2c254f0b54c784c49da7ab2abcfb341ef5c180b6
Yichuan Wang <133667618+Charlie-Wang-03@users.noreply.github.com>
2026-09-19 20:21:37 +0800

    JSP-000404: export hull turn slot budget

--- submissions/jsp-000404-wip-charlie/JSP000404Research.lean
+++ submissions/jsp-000404-wip-charlie/JSP000404Research.lean
@@ -59,6 +59,8 @@ import JSP000404Research.BoundaryDominance
 
 import JSP000404Research.BoundaryIntervalCount
 
+import JSP000404Research.HullTurnBudget
+
 import JSP000404Research.AdaptiveDirectionColor
 
 import JSP000404Research.DeficitTwo
