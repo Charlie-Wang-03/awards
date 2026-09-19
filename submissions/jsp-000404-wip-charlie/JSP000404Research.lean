843889484b946074b0babf79721bef03d2369e82
Yichuan Wang <133667618+Charlie-Wang-03@users.noreply.github.com>
2026-09-19 20:17:54 +0800

    JSP-000404: export one-exception boundary dominance

--- submissions/jsp-000404-wip-charlie/JSP000404Research.lean
+++ submissions/jsp-000404-wip-charlie/JSP000404Research.lean
@@ -53,6 +53,8 @@ import JSP000404Research.PhaseObstruction
 
 import JSP000404Research.BoundaryExcess
 
+import JSP000404Research.BoundaryDominance
+
 import JSP000404Research.AdaptiveDirectionColor
 
 import JSP000404Research.DeficitTwo
