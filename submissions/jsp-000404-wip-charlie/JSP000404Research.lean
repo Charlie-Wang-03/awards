4d58933248eb1ba5dad9f0abd9ff26cecb00e8c0
Yichuan Wang <133667618+Charlie-Wang-03@users.noreply.github.com>
2026-09-19 22:50:05 +0800

    JSP-000404: export antiperiodic sign path

--- submissions/jsp-000404-wip-charlie/JSP000404Research.lean
+++ submissions/jsp-000404-wip-charlie/JSP000404Research.lean
@@ -29,6 +29,8 @@ import JSP000404Research.StrictExposure
 
 import JSP000404Research.SignedRayMonodromy
 
+import JSP000404Research.BoolSignPath
+
 import JSP000404Research.LinearizedGap
 
 import JSP000404Research.CompensatedDeletion
